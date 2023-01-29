2fcf3d919abd3d93f91f2e25c42f86ee2977da42
Parv <pbhardwaj@primesoft.net>
2023-01-29 22:17:12 +0530

    Completed Task 4

--- Tasks/4_Abstract_Classes/Program.cs
+++ Tasks/4_Abstract_Classes/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public class Tester{
+    public static void Main(){
+        Console.WriteLine("Please enter name of your dog");
+        string nameOfDog = Console.ReadLine();
+
+        Dog dog = new Dog();
+        dog.Name = nameOfDog;
+        dog.Eat();
+
+    } 
+}
+
+public abstract class Animal{
+    public string Name {set; get;}
+
+    public abstract void Eat();
+}
+
+public class Dog : Animal{
+    public override void Eat(){
+        Console.WriteLine("{0} is eating.",this.Name);
+    }
+}
+
