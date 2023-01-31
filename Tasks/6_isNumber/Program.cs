1f840b2ed5eec6ba56e05bc1f6eca8cc109981a2
Parv <pbhardwaj@primesoft.net>
2023-01-31 21:06:48 +0530

    completed task 6

--- Tasks/6_isNumber/Program.cs
+++ Tasks/6_isNumber/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public class Tester {
+
+    public static void Main(string[] args){
+        Console.WriteLine("Please enter text");
+
+        string text = Console.ReadLine();
+
+        Console.WriteLine(isNumber(text));
+    }
+
+    public static bool isNumber(string text){
+        double n;
+        return double.TryParse(text, out n);
+    }
+}
