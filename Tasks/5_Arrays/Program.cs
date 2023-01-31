eb5d27ef9ed8ebd3b1849b46da4bc04c8304f269
Parv <pbhardwaj@primesoft.net>
2023-01-31 20:35:20 +0530

    completed task 5

--- Tasks/5_Arrays/Program.cs
+++ Tasks/5_Arrays/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public class Tester {
+
+    public static void Main(string[] args){
+        int[] numArray = new int[10];
+        for(int i =0; i<10;i++){
+            Console.WriteLine("Enter integer");
+            numArray[i] = int.Parse(Console.ReadLine());
+        }
+
+        for(int i=0; i<10; i++){
+            if (numArray[i] % 2 == 0){
+                Console.WriteLine(numArray[i]);
+            }
+        }
+    }
+}
