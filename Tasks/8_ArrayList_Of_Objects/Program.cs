74f4447c92f3ad1473908054e8c187fc88b88b1b
Parv <pbhardwaj@primesoft.net>
2023-02-03 16:08:11 +0530

    Task 8 Completed.

--- Tasks/8_ArrayList_Of_Objects/Program.cs
+++ Tasks/8_ArrayList_Of_Objects/Program.cs
@@ -1,2 +1,46 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections;
+
+public class Tester{
+    public static void Main(string[] args){
+
+        int TotalPersons = 3;
+
+        ArrayList List = new ArrayList();
+
+        for(int i=0;i<TotalPersons;i++){
+            Console.WriteLine("Enter name");
+            string name = Console.ReadLine();
+            Console.WriteLine("Enter age");
+            int age = int.Parse(Console.ReadLine());
+
+            Person p = new Person(name,age);
+            List.Add(p); 
+        }
+
+        for(int i =0; i<TotalPersons; i++){
+            Console.WriteLine(List[i].ToString());
+        }
+
+    }
+}
+
+public class Person{
+
+    public string Name {get; set;}
+    public int Age {get; set;}
+
+    public Person(string name, int age){
+        if(age <= 0){
+            throw new Exception("age has to be a postive number");
+        }
+
+        this.Name = name;
+        this.Age = age;        
+    }
+
+    public override string ToString()
+    {
+        return $"{this.Name} is {this.Age} years old";
+    }
+}
