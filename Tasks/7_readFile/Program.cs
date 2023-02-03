b4b5715cc8ebe03eb802d94b7a3637decb20f78e
Parv <pbhardwaj@primesoft.net>
2023-02-03 15:30:31 +0530

    Completed Task 7

--- Tasks/7_readFile/Program.cs
+++ Tasks/7_readFile/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public class Tester{
+
+    public static void Main(){
+        string FilePath = @"files/sample-text-file.txt";
+
+        Console.WriteLine("Reading file...");
+
+        string Text = File.ReadAllText(FilePath);
+
+        Console.WriteLine(Text);
+
+        Console.WriteLine("Coverting text to lower case.");
+
+        string TextInLowerCase = Text.ToLower();
+
+        Console.WriteLine("Writing Text in new text file");
+
+        string WriteFilePath = @"files/sample-text-file-2.txt";
+
+        File.WriteAllText(WriteFilePath,TextInLowerCase);
+    }
+}
