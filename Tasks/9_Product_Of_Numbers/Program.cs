103783ce3370529e8e150af9be9573a1608a823f
Parv <pbhardwaj@primesoft.net>
2023-02-03 16:16:43 +0530

    Completed task 9.

--- Tasks/9_Product_Of_Numbers/Program.cs
+++ Tasks/9_Product_Of_Numbers/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿    using System;
+    public class ProductOfNumber
+    {
+        public static void Main(string[] args)
+        {
+            int x = Convert.ToInt32(Console.ReadLine());
+            int y = Convert.ToInt32(Console.ReadLine());
+     
+            int result = 0;
+            int i = 0;
+     
+            while (i < y)
+            {
+                result = result + x;
+                i++;
+            }
+     
+            Console.WriteLine("Product of {0} and {1} is {2}", x, y, result);
+        }
+    }
