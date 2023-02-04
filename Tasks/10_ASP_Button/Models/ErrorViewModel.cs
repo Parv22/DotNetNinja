41d6f028789366a1adc6d0cb26627b66156a6961
Parv <pbhardwaj@primesoft.net>
2023-02-04 18:39:27 +0530

    Template set for task 10

---
+++ Tasks/10_ASP_Button/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace _10_ASP_Button.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
