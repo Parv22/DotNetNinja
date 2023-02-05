32446b429908609bceabf0336a1543801e9c3fa6
Parv <pbhardwaj@primesoft.net>
2023-02-05 15:28:24 +0530

    deleted

--- Tasks/10_ASP_Button/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace _10_ASP_Button.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
